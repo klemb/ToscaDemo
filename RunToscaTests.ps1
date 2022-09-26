313c08a5dcdab9564e874f3d4ad3e22b14c2b937
klemb <bernhard.klemm@virgin.net>
2022-09-26 13:32:29 +0100

    Update RunToscaTests.ps1

--- RunToscaTests.ps1
+++ RunToscaTests.ps1
@@ -1,2 +1,2 @@
 # & C:\temp\tosca_execution_client.ps1 -toscaServerUrl "http://localhost:85" -events '["The 16th"]' -projectName "DEX_Workspace" -resultsFolderPath "C:\temp" -resultsFileName  "Bernhard.xml"
-& ./tosca_execution_client.ps1 -toscaServerUrl "http://localhost:85" -events '["The 26th"]' -projectName "DEX_Workspace" -resultsFolderPath "./results" -resultsFileName  "Bernhard.xml" -d
+& ./tosca_execution_client.ps1 -toscaServerUrl "http://localhost:85" -events '["The 26th"]' -projectName "DEX_Workspace" -resultsFolderPath "./results" -resultsFileName  "Bernhard.xml" -d -enqueueOnly
